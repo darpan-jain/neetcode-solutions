547408d9fb0c5d7a73dc9da3238f732cce536c90
Darpan Jain <darpan.pmun@gmail.com>
2023-03-11 21:53:57 -0800

    Add solution for `Find Minimum in Rotated Array`

---
+++ Binary Search/min_in_rotated_array.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        '''
+        Use the Binary Search, while looking for the minimum.
+        Time Complexity: O(log n)
+        '''
+        
+        # Init two pointers to perform Binary Search
+        start, end = 0, len(nums) - 1
+
+        # Init result to +infinity (since we are looking for minimum)
+        minNum = float('inf')
+        
+        while start <= end:
+
+            # Edge case - when array is sorted! Just return the min value!
+            if nums[start] < nums[end]:
+                return min(minNum, nums[start])
+            
+            # Get the mid index
+            mid = (start + end) // 2
+
+            # Update the `minNum` by comparing with mid index element
+            minNum = min(minNum, nums[mid])
+            
+            ## Check which part you want to search in the rotated array
+            
+            # We compare the mid value and the leftmost value
+
+            # If mid is greater (we have overshot), usually we decrement right pointer (i.e. search left part), 
+            # BUT since array is rotated, we search the RIGHT portion as it will have smaller values.
+            if nums[mid] >= nums[start]:
+                start = mid + 1
+            
+            # Similarly, if not (nums[m] <= nums[l] is satisfied), we have undershot, we search 
+            # the LEFT portion (as opposed to usual right portion search), as now that will have smaller values.
+            else:
+                end = mid - 1
+                
+        return minNum
