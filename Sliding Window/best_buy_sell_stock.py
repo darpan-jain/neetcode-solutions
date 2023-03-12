d7169a70932ca85f9d3749c25193d2dfeb25d4e7
Darpan Jain <darpan.pmun@gmail.com>
2023-03-11 21:32:24 -0800

    Add solution for `Best time to Buy and Sell Stock`

---
+++ Sliding Window/best_buy_sell_stock.py
@@ -0,0 +1,20 @@
+'''
+Question: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+'''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Choose first element as buy price and initial profit as 0
+        buy, profit = prices[0], 0
+        
+        # Iterate over the prices but skip first element since that is assigned to current buy price
+        for curr_price in prices[1:]:
+            
+        # If you find a lower price than current buy price, that's your new buy price
+            if curr_price < buy: 
+                buy = curr_price
+                
+            # Keep updating profit to 'max b/w (curr buy price - previous buy price) and profit'
+            profit = max( (curr_price - buy), profit )
+
+        return profit
