a21ba6449a0f8321dc392eb114425fb63de08f52
Darpan Jain <darpan.pmun@gmail.com>
2023-03-24 16:21:19 -0700

    Uncomment class for `ListNode`

--- Linked List/merge_two_sorted_lists.py
+++ Linked List/merge_two_sorted_lists.py
@@ -3,10 +3,10 @@ Question: https://leetcode.com/problems/merge-two-sorted-lists/
 '''
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
--- Linked List/reverse_linked_list.py
+++ Linked List/reverse_linked_list.py
@@ -3,10 +3,10 @@ Question: https://leetcode.com/problems/reverse-linked-list/
 '''
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
