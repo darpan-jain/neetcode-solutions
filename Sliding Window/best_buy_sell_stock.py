0b8e5c812be17b3ec7f406561a906c529d068103
Darpan Jain <darpan.pmun@gmail.com>
2023-04-05 14:54:47 -0700

    Imports updated

--- Sliding Window/best_buy_sell_stock.py
+++ Sliding Window/best_buy_sell_stock.py
@@ -2,6 +2,8 @@
 Question: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 '''
 
+from typing import List
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # Choose first element as buy price and initial profit as 0
