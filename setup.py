7a3954ba5cc4968550b649638370e561903fa4c3
Darpan Jain <darpan.pmun@gmail.com>
2024-07-21 17:59:23 -0700

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='neetcode-solutions',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/darpan-jain/neetcode-solutions',
+    license='MIT',
+    author='Darpan Jain',
+    author_email='',
+    description='A collection of solutions to common Leetcode questions, based on a list compiled by neetcode.io.'
+)
