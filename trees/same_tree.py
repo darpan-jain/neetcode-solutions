bd1e3921fa8cfa76f91fe2788aec3109a9674ea4
Darpan Jain <darpan.pmun@gmail.com>
2024-07-24 16:17:33 -0700

    Update solution explanation and formatting fixes

--- trees/same_tree.py
+++ trees/same_tree.py
@@ -1,31 +1,40 @@
-'''
+"""
 Question: https://leetcode.com/problems/same-tree/
-'''
+"""
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+from typing import Optional
+
+
+class TreeNode:
+    """
+    Definition for a binary tree node.
+    """
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        '''
-        Use recursive DFS: Time Complexity = O(p + q)
-        '''
-        
-        ## Base cases
-        
-        # If both are null, then equal
+        """
+        Approach: Use Recursive DFS, with stopping conditions for when the nodes are null or their values are unequal.
+
+        Time Complexity: O(P+Q), where P and Q are the number of nodes in the two trees
+        """
+
+        # Base case for recursive call
+        # # If both are null, then equal
         if not p and not q:
             return True
-        
+
         # If only one of them null, then not equal OR both not null but have unequal values
         if (not p or not q) or (p.val != q.val):
             return False
-        
-        ## Recursive call
+
+        # Recursive call for DFS
         # Here, both p and q are not null and their values match,
         # so recursively check the left and right subtrees of each for equality
         return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
-    
