39c4a2ee5682b7d925bb51407031f8d7eeeb4516
Darpan Jain <darpan.pmun@gmail.com>
2025-07-13 10:45:17 -0700

    Add Divide and Conquer approach

--- greedy/maximum_subarray.py
+++ greedy/maximum_subarray.py
@@ -45,3 +45,53 @@ class Solution:
         # Return the final `maxSum`
         return maxSum
     
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        """
+        Approach 2: Divide and Conquer
+
+        Divide the array into two halves, recursively find the maximum subarray sum in each half,
+        and find the maximum subarray sum that crosses the midpoint.
+
+        Time Complexity: O(N log N), since search is done in `log N` time and we iterate through the array in each recursive call
+        Space Complexity: O(log N) for storing the recursive stack
+        """
+
+        # Empty or invalid case
+        if not nums:
+            return 0
+
+        # DFS function to find the max sum in each half of `nums`
+        def dfs(l, r):
+
+            # Base case: if left index is greater than right, return -infinity (since we want max sum)
+            if l > r:
+                return float("-inf")
+
+            # Find the mid index to divide `nums` into left and right
+            m = (l+r) // 2
+
+            # Init the currSum and leftSum
+            # Iterate over the left half starting from mid and going left until leftmost index `l-1` -> `m-1` to `l-1`
+            leftSum = currSum = 0
+            for i in range(m-1, l-1, -1):
+                # Add current elem to `currSum`
+                currSum += nums[i]
+                # Update leftSum to be the max of current leftSum and currSum
+                leftSum = max(leftSum, currSum)
+            
+            # Same for right half. Iterate from mid+1 and go right until rightmost index `r` -> `m+1` to `r+1`
+            rightSum = currSum = 0
+            for j in range(m+1, r+1):
+                currSum += nums[j]
+                rightSum = max(rightSum, currSum)
+            
+            '''
+            Perform recursive calls in left and right halves of the array (similar to Binary Search)
+            The function returns MAX between -
+                1. Recursive calls from left and right halves (which return max from their respective halves)
+                2. Sum of (leftSum, mid element, rightSum), from the current mid index `m`
+            '''
+            return max(dfs(l, m-1), dfs(m+1, r), leftSum + nums[m] + rightSum)
+        
+        return dfs(0, len(nums)-1)
