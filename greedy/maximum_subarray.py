98f67bb0682e8f1fcaae333e17208c035e8b158d
Darpan Jain <darpan.pmun@gmail.com>
2025-07-13 10:27:58 -0700

    Update comments and implementation logic

--- greedy/maximum_subarray.py
+++ greedy/maximum_subarray.py
@@ -2,45 +2,46 @@
 Question: https://leetcode.com/problems/maximum-subarray/
 '''
 
+from typing import List
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:        
-        ''' Approach 1: 
-         Kadane's Algorithm: Time Complexity = O(n)
-         
-         - If the sum of a subarray is positive, it is possible to 
-         make the next value bigger, so we keep do it until it turns negative.     
-         - If the sum is negative, it has no use to the next element, so we break.
-         It is a game of sum, not the elements.
-        '''
+        """
+        Approach 1: Kandane's Algorithm
 
+        Iterate through `nums`, adding each element to the `currSum`
+        Ensure that `currSum` is never non-zero (negative)
+
+        - If the sum of a subarray is positive, it is possible to 
+        make the next value bigger, so we keep doing it until it turns negative.     
+        - If the sum is negative, it has no use to the next element, so we break.
+        
+        It is a game of sum, not the elements!
+
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+        """
+
+        # Empty or invalid case
         if not nums:
             return 0
-        
-        # for i in range(1, len(nums)):
-        #     # Add to current sum only if the value is positive
-        #     if nums[i-1] > 0:
-        #         nums[i] += nums[i-1]
 
-        # return max(nums)
-    
-        ''' Approach 2: (same idea) Time Complexity = O(n) '''
-        
-        # Init current sum to 0 and maxSum to first element
-        currSum = 0
+        # Initialize maxSum as the first element and currSum as 0
         maxSum = nums[0]
-        
-        # Iterate over the remaining elements
-        for n in nums:
-            # Add current number to currSum
-            currSum += n
-            # and compare with existing maxSum
-            maxSum = max(currSum, maxSum)
-            
-            # If currSum goes negative, make the currSum zero
-            # since we cannot use a subarray with negative values (larger than currSum)
-            # as they will result in a negative sum (obvs won't be max)
+        currSum = 0
+
+        # Iterate over all elements in `nums`
+        for num in nums:
+            # For each iteration, ensure that `currSum` is always non-zero
             if currSum < 0:
                 currSum = 0
             
+            # Add current element `num` to `currSum`
+            currSum += num
+
+            # Update `maxSum` for every iteration
+            maxSum = max(maxSum, currSum)
+        
+        # Return the final `maxSum`
         return maxSum
     
