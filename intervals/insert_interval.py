a01476448884dc4bb537924300c587129f1599dc
Darpan Jain <darpan.pmun@gmail.com>
2025-07-26 20:36:20 -0400

    Update description and comments explanation for clarity

--- intervals/insert_interval.py
+++ intervals/insert_interval.py
@@ -6,16 +6,18 @@ from typing import List
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        """
-        Approach: 
-            When inserting, check if they are merging.
-            If they are, the new interval is the min of starts and max of ends
+        """ 
+        Approach: Three cases for handling `newInterval` when adding to `intervals`
+            - To be added before an interval (no overlap)
+            - To be added after an interval (no overlap)
+            - To be merged with another interval (overlap with another interval)
 
-        How to check where to add the new interval (in case of no-overlap)?
-                                     IF
-            - end of new interval < start of curr interval, then add before 
-                                  ELSE IF
-            - start of new interval > end of curr interval, then add after
+        For no overlap:
+            - IF end of new interval < start of curr interval -> ADD BEFORE
+            - IF start of new interval > end of curr interval -> ADD AFTER
+        
+        For overlap:
+            New Interval boundaries = min of starts & max of ends
         
         Time Complexity: O(N)
         Space Complexity: O(N)
@@ -23,12 +25,13 @@ class Solution:
         
         res = []
         
+        # Iterate through all intervals
         for i in range(len(intervals)):
             
             # Extract the start and end of current interval
             curr_start, curr_end = intervals[i]
 
-            # No overlap, to be merged before current interval
+            # No Overlap, merge BEFORE current interval
             # Check if the new interval can be merged BEFORE the current interval
             if newInterval[1] < curr_start:
                 # We append the newInterval to the result first
@@ -38,21 +41,20 @@ class Solution:
                 # Now we don't need to check, so we return the result
                 return res + intervals[i:]
             
-            # No overlap, new to be merged AFTER current interval
+            # No Overlap, merge AFTER current interval
             # Checks if the interval can be merged after the end of the current interval
             elif newInterval[0] > curr_end:
-                # Here we add the current interval, and keep checking since the newInterval
-                # could be merged after other intervals too!
+                # Here we only add current interval to `res` and keep checking
+                # since the newInterval can be merged after or overlap with other intervals too!
                 # So we only append the current interval, and keep going forward.
                 res.append(intervals[i])
             
-            # Overlap case, so we create the new merged interval using 
-            # -> min of starts and max of ends
+            # Overlap, create new merged interval boundaries ->  min of starts and max of ends
             else:
                 newInterval = [ min(newInterval[0], curr_start),
                                 max(newInterval[1], curr_end) ]
             
-        # We add the new merged interval to the result (for elif and else cases)
+        # Finally, add the new merged interval to `res` (outside of the for loop), 
+        # since not done during the No Overlap+Merge After and the Overlap+New Interval Boundaries cases
         res.append(newInterval)
-            
         return res
