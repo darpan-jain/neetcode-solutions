c1c41724b623bbb6a01d507f7bbdc177e1e60f8a
Darpan Jain <darpan.pmun@gmail.com>
2025-11-23 18:57:32 -0800

    Update solution and add comments

--- 1d_dynamic_programming/palindromic_substrings.py
+++ 1d_dynamic_programming/palindromic_substrings.py
@@ -4,21 +4,29 @@ Question: http://www.lintcode.com/en/problem/palindromic-substrings/
 
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        '''
-        Approach 1: Count even and odd in different loops and combine the results from both
-        Time Complexity: Linear O(n) (recheck but pretty sure it's correct!)
-        '''
-        numPalis = 0
-        n = len(s)
+        """
+        Approach 1: Same approach as https://leetcode.com/problems/longest-palindromic-substring/,
+        but instead of keeping track of the longest palindrome, we just count the number of palindromes we find.
+        Remember that there are multiple palindromes that can be formed by expanding outwards from the same midpoint, so we need to keep track of all of them
+        Eg. "aaa" -> "a", "a", "a", "aa", "aa", "aaa" -> 6 palindromic substrings
+        
+        Time Complexity: O(N^2) - we are iterating through all the characters in the string and for each character, we are expanding outwards to find palindromes
+        Space Complexity: O(1) - we are not using any extra space, just a few variables to keep track of the count and pointers
+        """
+        
+        # Result variable to keep track of the number of palindromic substrings
+        num_palindromes = 0
         
         # Count odd length palidromes
-        for i in range(n):
+        for i in range(len(s)):
+
             # For odd length palindromes -> l == r == i
-            numPalis += self.count_palindromes_by_expansion(i, i, s)
+            num_palindromes += self.count_palindromes_by_expansion(i, i, s)
+
             # For even length palindromes -> l = i and  r = i+1
-            numPalis += self.count_palindromes_by_expansion(i, i+1, s)
+            num_palindromes += self.count_palindromes_by_expansion(i, i+1, s)
             
-        return numPalis
+        return num_palindromes
     
     # Helper function to find palindromes between two pointers `l` and `r`, given a string `s`
     def count_palindromes_by_expansion(self, l, r, s):
@@ -30,27 +38,5 @@ class Solution:
             l -= 1
             r += 1
         
+        # Final count contains all possible palindromes formed by expanding outwards from `l` and `r`
         return count
-    
-        '''
-        Approach 2: Count even and odd length palindromes in the same loop - Same logic, seems a little more complicated
-        '''
-#         N = len(s)
-#         result = 0
-        
-#         # Iterate through all substrings of `s`
-#         # Why 2N?
-#         for i in range(2*N-1):
-#             # Find the mid of the current substring and move in opposite directions
-#             left = i // 2 # mid
-#             right = left + (i % 2) # mid + 1 for even string, and mid for odd string
-        
-#             # Loop to find palindrome strings using `left` and `right` i.e. mid and mid-1
-#             while left >= 0 and right < N and s[left] == s[right]:
-#                 result += 1
-#                 left -= 1
-#                 right += 1
-        
-#         # Result is the total number of palindrome substrings
-#         return result
-    
