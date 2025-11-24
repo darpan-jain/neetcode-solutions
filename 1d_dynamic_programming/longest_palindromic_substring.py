d0923827ec4638a501c731867b7856082eb4dc16
Darpan Jain <darpan.pmun@gmail.com>
2025-11-23 18:49:01 -0800

    Update comments and formatting

--- 1d_dynamic_programming/longest_palindromic_substring.py
+++ 1d_dynamic_programming/longest_palindromic_substring.py
@@ -4,12 +4,19 @@ Question: https://leetcode.com/problems/longest-palindromic-substring/
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        '''
-        Check for palindrome by starting in the middle and expanding outwards. 
-        We do this by mid = ith index in the string ('i' is each char in the string).
-        Remember to check for odd and even both, since the mid will be different in both cases.
-        '''
+        """
+        Approach: 
         
+            - Check for palindrome at each position in the string, starting in the middle and expanding outwards
+            - Done by considering mid as the ith index in the string ('i' is each char in the string), where i is the current position in string's iteration
+            - Have to Remember to check for both odd and even length palindromes, i.e. when the middle element is same and when the middle element is different (i.e. two middle elements)
+
+        Time Complexity: O(N^2) -> We are iterating through the string and for each character we are checking for palindrome by expanding outwards
+                                   In worst case, we will check for all characters in the string
+        Space Complexity: O(1) -> We are using constant space to store the longest palindrome string
+        """
+        
+        # Define the longest palindrome string variable to store the result
         longest = ""
         
         for i in range(len(s)):
@@ -20,7 +27,7 @@ class Solution:
             odd = self.palindrome_by_expansion(i, i, s)
             
             # For even length palindromes -> l = i and  r = i+1
-            even = self.palindrome_by_expansion(i, i+1, s)
+            even = self.palindrome_by_expansion(i, i + 1, s)
             
             # Update the `longest` palindrome string from odd and even checks
             longest = max(longest, even, odd, key=len)            
@@ -28,15 +35,16 @@ class Solution:
         # Return the result
         return longest
     
-    # Helper method to find palindrom between two pointers `l` and `r` in string `s`
+    # Helper method to find the longest palindrome between two pointers `l` and `r` in string `s`, by expanding outwards from the middle
     def palindrome_by_expansion(self, l, r, s):
+
         # Iterate with two pointers outwards, until the characters don't match i.e. end of palindromic substring
         # Start in the middle and move pointers in opposite directions
         while l >= 0 and r < len(s) and s[l] == s[r]:
             l -= 1
             r += 1
         
-        # When the comparison stops, you return the palidrome string
-        # i.e. the string in between `l` and `r` pointers
+        # When the comparison stops, you return longest palidrome string using current `l` and `r` 
+        # i.e. the string in between `l` and `r` pointers (which are now at the ends of the palindromic substring)
         # Excludes the last location of l and r, i.e, l+1 and r-1 
-        return s[l+1:r]
+        return s[l + 1:r]
